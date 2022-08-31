1ce8547a6013576b1cac5b31b988d9a5f8247f32
alagappan17 <alagappan1703@gmail.com>
2022-08-31 12:18:05 +0530

    Images change on button click

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,23 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top Text" />
                 <input type="text" className="form--input" placeholder="Bottom Text" />
-                <button className="form--button">Get new meme image</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get new meme image</button>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
